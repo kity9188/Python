b5f1dd70f25180789ee0da9f8078c5665df4226c
Nicketa <nicks9188@gmail.com>
2019-02-07 15:18:41 +0530

    Intro

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+print('Hello, World!')
+print(1+2)
+print(7*6)
+print()
+print("The End")
+print("Python's strings are easy to use.")
+print('We can even include "quotes" in strings.')
+print("Python doesn't mind if we'r using single or double quote./n It works in both ways")
+#for newline, /n doesn't works here.
+print("Hello" + 'World')
+greeting = "Hello"
+name = "Nicks"
+print(greeting + name)
+#If we want a space, we can add that too.
+print(greeting +' ' +name)
